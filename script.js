a22d1b25f3b59efb860fa1a950f5e7516682f5b2
Sonjoy Dey <95380764+sonjoy2074@users.noreply.github.com>
2023-11-08 18:41:00 +0600

    local stroge

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ form.addEventListener('submit',addTask);
 taskList.addEventListener('click',removeTask);
 clearBtn.addEventListener('click',clearTask);
 filter.addEventListener('keyup',filterTask);
+document.addEventListener('DOMContentLoaded',getTasks);
 //Define functions
 //add task
 function addTask(e){
@@ -34,6 +35,7 @@ function removeTask(e){
       if(confirm("Are you sure?")){
           let ele=e.target.parentElement;
           ele.remove();
+          removeFromLS(ele);
       }
   }
 
@@ -42,7 +44,8 @@ function clearTask(e){
     //taskList.innerHTML='';
     while(taskList.firstChild){
         taskList.removeChild(taskList.firstChild);
-    }   
+    }  
+    localStorage.clear(); 
 
 }
 function filterTask(e){
@@ -66,3 +69,36 @@ function storeTaskInLocalStorage(task){
     tasks.push(task);
     localStorage.setItem('tasks',JSON.stringify(tasks));
 }
+function getTasks(){
+    let tasks;
+    if(localStorage.getItem('tasks')===null){
+        tasks=[];
+    }else{
+        tasks=JSON.parse(localStorage.getItem('tasks'));
+    }
+    tasks.forEach(task=>{
+        let li=document.createElement('li');
+        li.appendChild(document.createTextNode(task+" "));
+        let link=document.createElement('a');
+        link.setAttribute('href','#');
+        link.innerHTML='x';
+        li.appendChild(link);
+        taskList.appendChild(li);
+    });
+}
+function removeFromLS(taskItem){
+    let tasks;
+    if(localStorage.getItem('tasks')===null){
+        tasks=[];
+    }else{
+        tasks=JSON.parse(localStorage.getItem('tasks'));
+    }
+    let li=taskItem;
+    li.removeChild(li.lastChild);
+    tasks.forEach((task,index)=>{
+        if(li.textContent.trim()===task){
+            tasks.splice(index,1);
+        }
+    });
+    localStorage.setItem('tasks',JSON.stringify(tasks));
+} 
